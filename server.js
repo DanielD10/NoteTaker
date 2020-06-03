a1d59f96814374c186f488575629190d5cf68404
Daniel Duran Jr <danielduranjr@Daniels-Air.attlocal.net>
2020-06-03 12:00:46 -0500

    add server

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+
+// const apiRoutes = require("./routes/apiRoutes");
+// const htmlRoutes = require("./routes/htmlRoutes");
+
+// Initialize the app and create a port
+const app = express();
+const PORT = process.env.PORT || 3030;
+
+// Set up body parsing, static, and route middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+const htmlRoutes = require("./routes/htmlRoutes.js");
+const apiRoutes = require("./routes/apiRoutes.js");
+
+app.use(htmlRoutes);
+app.use(apiRoutes);
+
+// Start the server on the port
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
